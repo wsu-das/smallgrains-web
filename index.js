4d6070854d2e0b454bf5cd6157771798048a1434
Izzy McCabe <izzym@pacificbird.dev>
2025-10-21 11:06:03 -0700

    responsive nav menu and grid

--- index.js
+++ index.js
@@ -16,25 +16,36 @@ export const toDashCase = (title) => title.toLowerCase().replace(' ', '-');
 customElements.define("das-sg-shell", class extends HTMLElement {
   constructor() {
     super();
+
+    const links = html`
+      <ul>
+        <li hx-boost="true" id="variety-selector" href="/">
+          <img src="/resources/img/das-us-logo-white-45h.png" alt="" href="" i="">
+        </li>
+        <li> <a hx-boost="true" id="variety-selector" href="/variety-selector">Variety Selector</a></li>
+        <li> <a hx-boost="true" id="nitrogen-fertilizer" href="/nitrogen">Nitrogen Fertilizer</a></li>
+        <li> <a hx-boost="true" id="lime-requirement" href="/lime">Lime Requirement</a></li>
+        <li> <a hx-boost="true" id="spring-canola" href="/canola">Spring Canola</a></li>
+        <li> <a hx-boost="true" id="straw-removal" href="/straw-removal">Straw Removal</a></li>
+      </ul>
+    `;
+    
     this.outerHTML = html`
       <header>
-        <menu class="float-right">
-          <li><button class="contrast">
-              <p>Home</p>
-            </button></li>
+        <button
+          popovertarget="mobile-menu"
+          aria-label="Show Site Menu"
+          aria-controls="mobile-menu"
+        ></button>
       </header>
-      <aside>
+      <aside id="desktop-menu">
         <nav>
-          <ul>
-            <li hx-boost="true" id="variety-selector" href="/">
-              <img src="/resources/img/das-us-logo-white-45h.png" alt="" href="" i="">
-            </li>
-            <li> <a hx-boost="true" id="variety-selector" href="/variety-selector">Variety Selector</a></li>
-            <li> <a hx-boost="true" id="nitrogen-fertilizer" href="/nitrogen">Nitrogen Fertilizer</a></li>
-            <li> <a hx-boost="true" id="lime-requirement" href="/lime">Lime Requirement</a></li>
-            <li> <a hx-boost="true" id="spring-canola" href="/canola">Spring Canola</a></li>
-            <li> <a hx-boost="true" id="straw-removal" href="/straw-removal">Straw Removal</a></li>
-          </ul>
+          ${links}
+        </nav>
+      </aside>
+      <aside id="mobile-menu" popover>
+        <nav>
+          ${links}
         </nav>
       </aside>
     `;
